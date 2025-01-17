c33805b4364716ecedf1fb11e4196ae34072f3bf
Afrin Nahar <afrinnahar1999@gmail.com>
2025-01-17 14:04:56 +0600

    added see more functionality on leaderboard

--- src/components/sections/Leaderboard.tsx
+++ src/components/sections/Leaderboard.tsx
@@ -1,7 +1,15 @@
-import leaderboardData from "../../assets/data/leaderboard.json"
-import LeaderboardCard from "../LeaderboardCard"
+import { useState } from "react";
+import leaderboardData from "../../assets/data/leaderboard.json";
+import LeaderboardCard from "../LeaderboardCard";
+import { Button } from "../ui/button";
 
 const Leaderboard = () => {
+  const [visibleCount, setVisibleCount] = useState<number>(5);
+
+  const handleSeeMore = () => {
+    setVisibleCount((prevCount) => prevCount + 5);
+  };
+
   return (
     <div className="px-4 font-sans text-gray-800">
       <div className="text-center w-full flex flex-col gap-4 items-center justify-center py-8">
@@ -16,10 +24,21 @@ const Leaderboard = () => {
 
       {/* <div className="flex flex-wrap gap-4 my-6 justify-center"> */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
-        {leaderboardData && leaderboardData.map((data, index) => (
+        {leaderboardData.slice(0, visibleCount).map((data, index) => (
           <LeaderboardCard key={index} data={data} />
         ))}
       </div>
+
+      {visibleCount < leaderboardData.length && (
+        <div className="flex justify-center my-6">
+          <Button variant="ghost"
+            className="px-6 py-2 text-darkGreen hover:bg-transparent hover:text-darkGreen/80 transition duration-200"
+            onClick={handleSeeMore}
+          >
+            See More
+          </Button>
+        </div>
+      )}
 
     </div>
   )
