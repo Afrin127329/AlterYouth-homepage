67f722557d4481ae1cefac4bc23028ce331868c4
Afrin Nahar <afrinnahar1999@gmail.com>
2025-01-18 18:45:17 +0600

    completed section1 of eligibility

--- src/components/sections/Eligibility.tsx
+++ src/components/sections/Eligibility.tsx
@@ -1,7 +1,51 @@
+import SvgDisabledFather from "@/assets/icons/DisabledFather"
+import SvgOrphan from "@/assets/icons/Orphan"
+import SvgSingleMother from "@/assets/icons/SingleMother"
 
 const Eligibility = () => {
   return (
-    <div>Eligibility</div>
+    <div className="font-sans">
+      <div className="section1">
+        <div className="flex flex-col justify-center items-center my-8">
+          <h2 className="text-2xl font-bold mb-4">Scholarship Eligibility</h2>
+          <p className="text-center w-full md:w-[550px]">Only students who are currently enrolled in Government Primary Schools and fall
+            under any of the following criteria are eligible to apply for scholarships</p>
+        </div>
+        <div className="flex gap-6 items-center justify-center flex-wrap my-8">
+          <div className="flex gap-4 items-center leaderboard-box-shadow p-6 rounded-lg max-w-[375px] w-full">
+            <SvgDisabledFather color="#1dc468" width={40} />
+            <div>
+              <h2 className="font-semibold mb-1">Disabled Father</h2>
+              <p>Student's father is physically unable
+                to work</p>
+            </div>
+          </div>
+          <div className="flex gap-4 items-center leaderboard-box-shadow p-6 rounded-lg max-w-[375px] w-full">
+            <SvgSingleMother color="#1dc468" width={40} />
+            <div>
+              <h2 className="font-semibold mb-1">Single Mother</h2>
+              <p>Student's father is deceased or has abandoned the family</p>
+            </div>
+          </div>
+          <div className="flex gap-4 items-center leaderboard-box-shadow p-6 rounded-lg max-w-[375px] w-full">
+            <SvgOrphan color="#1dc468" width={40} />
+            <div>
+              <h2 className="font-semibold mb-1">Orphan</h2>
+              <p>Both parents are deceased or have abandoned the child</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section2">
+
+      </div>
+      <div className="section3">
+
+      </div>
+      <div className="section4">
+
+      </div>
+    </div>
   )
 }
 
--- src/components/sections/Nav.tsx
+++ src/components/sections/Nav.tsx
@@ -26,7 +26,7 @@ const Nav = () => {
     <nav className={`font-sans fixed top-0 left-0 right-0 z-50 ${active ? "bg-white text-darkGreen" : "bg-transparent text-white"} transition-all duration-300 ease-in-out`}>
       <Sidebar menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
 
-      <section className="flex justify-between items-center p-4">
+      <section className="flex justify-between items-center p-4 leaderboard-box-shadow">
         <div className="flex gap-4 items-center mx-8">
           <button onClick={() => setMenuIsOpen(!menuIsOpen)} className="mt-1">
             <svg className="w-5 h-5" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
